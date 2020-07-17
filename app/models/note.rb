993b4308896a743dab0ccadf116d7557d9f5cba2
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-17 20:08:18 +0000

    Automatically backed up by Learn

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,3 @@
-class Note < ActiveRecord::Base
-  # add associations here
+class Notes <  ActiveRecord::Base
+  belongs_to :song
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,26 @@
 class Song < ActiveRecord::Base
-  # add associations here
+  belongs_to :artist
+  belongs_to :genres
+  has_many :notes
+
+  def artist_name=(name)
+      self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_id=(name)
+    genre = Genre.find_by(name: name)
+   self.genre_id = genre.id
+    end
+
+  def notes=(notes)
+    notes.each do |note|
+      new_note = Note.create(content: note)
+      self.notes << new_note
+    end
+  end
+
 end
